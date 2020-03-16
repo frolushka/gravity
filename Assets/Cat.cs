55a3d02a623233cdbaea82319cd462a693e2f3b3
frolushka <mpfrolovstud@gmail.com>
2020-03-16 22:42:20 +0300

    Add cat and sounds

---
+++ Assets/Cat.cs
@@ -0,0 +1,18 @@
+﻿using UnityEngine;
+
+public class Cat : MonoBehaviour
+{
+    [SerializeField] private AudioClip audioClip;
+    [SerializeField] private Vector2 pitch;
+
+    private void OnCollisionEnter2D(Collision2D other)
+    {
+        var go = new GameObject();
+        var audioSource = go.AddComponent<AudioSource>();
+        audioSource.playOnAwake = false;
+        audioSource.clip = audioClip;
+        audioSource.pitch = Random.Range(pitch.x, pitch.y);
+        audioSource.Play();
+        Destroy(go, .7f);
+    }
+}
